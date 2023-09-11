0bc990f3f6c4d1c11646d12eba0ca3dbb3164b07
Nurtilek Sydykovich <nurtileks2005@gmail.com>
2023-09-11 12:55:29 +0600

    update

--- homeWork1/src/Main.java
+++ homeWork1/src/Main.java
@@ -7,8 +7,53 @@
 Для полученного решения укажите сложность в О-нотации.
 ------------------------------------------------------------------------------------------ */
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+    
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main program = new Main();
+        program.run();
+    }
+
+    public void run() {
+        String message = "Введите число N: ";
+        int number1 = this.promptNumber(message);
+        this.run(number1);
+    }
+
+    public void run(int number1) {
+        int[] divisorsCount = new int[1000001];
+        for (int i = 1; i <= 1000000; i++) {
+            for (int j = i; j <= 1000000; j += i) {
+                divisorsCount[j]++;
+            }
+        }
+
+        for (int i = 0; i < number1; i++) {
+            int Ai = this.promptNumber();
+            System.out.println(divisorsCount[Ai]);
+        }
+    }
+
+
+    public int promptNumber() {
+        return this.promptNumber("Enter number or enter 'exit': ");
+    }
+
+    public int promptNumber(String message) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(message);
+            String line = iScanner.nextLine();
+            if (line.equalsIgnoreCase("exit")) {
+                throw new NoSuchElementException();
+            }
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException exception) {
+                System.out.println("Incorrect number!");
+            }
+        }
     }
 }
--- homeWork2/src/Main.java
+++ homeWork2/src/Main.java
@@ -4,8 +4,110 @@
 Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
 -------------------------------------------------------------- */
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main program = new Main();
+//        program.run(new int[]{32, 45, 21, 91, 18, 8});
+        program.run();
+    }
+
+    public void run() {
+        int[] array = this.promptArray();
+        this.run(array);
+    }
+
+    public void run(int[] array) {
+        System.out.println("Input array: " + Arrays.toString(array));
+        System.out.println("Heap sorted array: " + Arrays.toString(this.heapSort(array)));
+    }
+
+
+    public int[] promptArray() {
+        return this.promptArray("Enter array values");
+    }
+
+
+    public int[] promptArray(String message) {
+        return this.promptArray(message, -1);
+    }
+
+    public int[] promptArray(int size) {
+        return this.promptArray("Enter array values", size);
+    }
+
+    public int[] promptArray(String message, int size) {
+        int index = 1;
+        int[] array = new int[]{};
+        System.out.println(message);
+        while (size >= index || size == -1) {
+            try {
+                int number = this.promptNumber("Enter value (%d/%s) or enter 'exit': ".formatted(index, size));
+                array = Arrays.copyOf(array, index);
+                array[index - 1] = number;
+                index++;
+            } catch (NoSuchElementException exception) {
+                break;
+            }
+        }
+        return array;
+    }
+
+
+    public int promptNumber() {
+        return this.promptNumber("Enter number or enter 'exit': ");
+    }
+
+    public int promptNumber(String message) {
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(message);
+            String line = iScanner.nextLine();
+            if (line.equalsIgnoreCase("exit")) {
+                throw new NoSuchElementException();
+            }
+            try {
+                return Integer.parseInt(line);
+            } catch (NumberFormatException exception) {
+                System.out.println("Incorrect number!");
+            }
+        }
+    }
+
+    public int[] heapSort(int[] arr) {
+        int size = arr.length;
+        int[] sorted = Arrays.copyOf(arr, size);
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            this.heapify(sorted, size, i);
+        }
+        for (int i = size - 1; i >= 0; i--) {
+            int temp = sorted[0];
+            sorted[0] = sorted[i];
+            sorted[i] = temp;
+            this.heapify(sorted, i, 0);
+        }
+        return sorted;
+    }
+
+    public void heapify(int[] arr, int size, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < size && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < size && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            this.heapify(arr, size, largest);
+        }
     }
 }
